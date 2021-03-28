91113e1ccf17373aef37f339c9b9feee523135aa
Nivetheni <62382531+Nivetheni@users.noreply.github.com>
2021-03-28 16:29:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import pickle
+from keras.models import model_from_json
+import pandas as pd
+import datetime
+import re
+
+from final_file import final
+# https://www.tutorialspoint.com/flask
+import flask
+app = Flask(__name__)
+
+final_object=final()
+###################################################
+
+###################################################
+
+
+@app.route('/',methods=['GET'])
+def home():
+    return flask.render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    index=request.form.to_dict()['Enter_index']
+    date=request.form.to_dict()['Enter_date']
+    client,access,language,predicted,time=final_object.predict(index,date)
+    return flask.render_template('new.html',Client=client,Access=access,Language=language,predicted=predicted,time=time)
+
+if __name__ == '__main__':
+    app.run(debug=True)
